f64edb94272bdbafe854fe2ed593d2df84ba3162
unknown <dattaguest@sd-2210994-w.dattaweb.com>
2022-02-20 22:53:59 -0300

    Se modifica template

--- django_init/aplicaciones/principal/views.py
+++ django_init/aplicaciones/principal/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
+    criterios=Criterio.objects.all()
+    websites=Website.objects.all()
+    contexto={'criterios':criterios,'websites':websites}
     if request.method=='GET':
-        print("Invocacion via GET")
-        criterios=Criterio.objects.all()
-        websites=Website.objects.all()
-        print(criterios)
-        contexto={'criterios':criterios,'websites':websites}
+        print("Invocacion via GET")   
+     
         return render(request,'index.html',contexto)
     else:
           if request.method=='POST':
@@ -19,7 +19,7 @@ def inicio(request):
                  print("Recuperando valor")
                  print(request.POST.get("Prueba1"))
                  print("invocaste a un post")
-                 return render(request,'index.html')
+                 return render(request,'index.html',contexto)
  # items=Criterio.objects.all()
  # for item in items:
  #     print(item.nombre)
