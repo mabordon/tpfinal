7f7c9d6a1d8c54c873ef6cf0e78fc2ac211e1bc1
unknown <dattaguest@sd-2210994-w.dattaweb.com>
2022-05-15 21:08:24 -0300

    Se agrega analizador de topicos

---
+++ topicanalizer.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+
+classifier = pipeline("zero-shot-classification")
+
+def analyze(sequence,candidate_labels):
+   result=classifier(sequence, candidate_labels)
+   return result
+
+if __name__=='__main__':
+   sequence = "The managers are so rude"
+   candidate_labels = ["salary", "work-environment", "bosses"]
+   result=analyze(sequence,candidate_labels)
+   print(result)
